46684759eeeee48276874da4cb4358c30bdd3a60
James Cooper <jamescooper@gmail.com>
2017-09-30 21:03:48 -0400

    Started monofunctor lifting

--- Functors.thy
+++ Functors.thy
@@ -14,6 +14,23 @@ definition bifunctor :: "(type \<Rightarrow> type \<Rightarrow>  type) \<Rightar
        \<and> f \<epsilon> \<epsilon> \<simeq> \<epsilon> 
        \<and> (\<forall>e\<^sub>1\<^sub>1 e\<^sub>1\<^sub>2 e\<^sub>2\<^sub>1 e\<^sub>2\<^sub>2. f (e\<^sub>1\<^sub>1 \<cdot> e\<^sub>1\<^sub>2) (e\<^sub>2\<^sub>1 \<cdot> e\<^sub>2\<^sub>2) \<simeq> f e\<^sub>1\<^sub>1 e\<^sub>2\<^sub>1 \<cdot> f e\<^sub>1\<^sub>2 e\<^sub>2\<^sub>2))"
 
+theorem identity_monofunctor: "monofunctor id id"
+  by (simp add: monofunctor_def)
+
+theorem const_monofunctor: "monofunctor (\<lambda>_. t) (\<lambda>_. \<epsilon>)"
+  proof -    
+    have "\<epsilon> \<cdot> \<epsilon> \<simeq> \<epsilon>" by simp
+    hence "\<epsilon> \<simeq> \<epsilon> \<cdot> \<epsilon>" by (metis equiv_sym)
+    thus ?thesis by (simp add: monofunctor_def)
+  qed
+
+theorem lifted_monofunctor: "monofunctor F f \<Longrightarrow> monofunctor G g \<Longrightarrow> monofunctor (G o F) (g o f)"
+  by (simp add: monofunctor_def)
+
+theorem lifted_bifunctor: "monofunctor F f \<Longrightarrow> monofunctor G g \<Longrightarrow> bifunctor H h \<Longrightarrow> 
+    monofunctor (\<lambda>t. H (G t) (F t)) (\<lambda>e. h (g e) (f e))"
+  by (simp add: monofunctor_def bifunctor_def)
+
 theorem prod_bifunctor: "bifunctor (op \<otimes>) (op \<parallel>)"
   by (simp add: bifunctor_def)
 
